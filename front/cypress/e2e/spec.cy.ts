82b909655444b74a91ec2f3c97ee7bdb456dff2b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-06-15 16:32:36 +0200

    dev: test integration

--- front/cypress/e2e/spec.cy.ts
+++ front/cypress/e2e/spec.cy.ts
@@ -1,6 +1,20 @@
 describe('My First Test', () => {
   it('Visits the initial project page', () => {
-    cy.visit('/')
-    cy.contains('app is running!')
-  })
-})
+    cy.visit('/');
+    cy.contains('Gestion Stock');
+    cy.contains('Mentions Légales');
+    cy.contains('a', 'Voir le stock').click();
+    cy.get('a[title="Ajouter"]').click();
+
+    const uuid = () => Cypress._.random(0, 1e6);
+    const id = uuid();
+    const name = `outil-${id}`;
+    cy.focused().clear().type(name);
+    cy.get('input').eq(1).clear().type('1.23');
+    cy.get('input').eq(2).clear().type('56');
+    cy.contains('form button', 'Ajouter').click();
+    cy.contains('tbody tr td', name).click();
+    cy.get('button[title="Supprimer"]').click();
+    cy.contains('tbody tr td', name).should('not.exist');
+  });
+});

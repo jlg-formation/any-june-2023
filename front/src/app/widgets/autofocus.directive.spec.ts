bdbb0a18a4c4af88fefc24a322a87bd3cf9ac88d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-06-15 15:11:09 +0200

    dev: test

--- front/src/app/widgets/autofocus.directive.spec.ts
+++ front/src/app/widgets/autofocus.directive.spec.ts
@@ -1,5 +1,26 @@
+import { Component } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { AutofocusDirective } from './autofocus.directive';
+
+@Component({
+  selector: 'app-test',
+  template: `<input appAutofocus />`,
+})
+export class TestComponent {}
+
 describe('AutofocusDirective', () => {
-  it('should create an instance', () => {
-    expect(true).toBeTruthy();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [TestComponent, AutofocusDirective],
+    }).compileComponents();
+  });
+
+  it('should autofocus', () => {
+    const fixture = TestBed.createComponent(TestComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    const inputElt = compiled.querySelector('input');
+    expect(document.activeElement).toBe(inputElt);
   });
 });
